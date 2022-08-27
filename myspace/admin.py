21aa5578ae28ffc5a32bd3d120771501f268f3e7
desousamario85 <desousa.mario@yahoo.com>
2022-08-27 19:32:02 +0000

    Update admin.py with all tables admin.site.register

--- myspace/admin.py
+++ myspace/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+# Import Data Tables
 
 from .models import Office_Spaces
 from .models import Category
@@ -8,3 +8,12 @@ from .models import STATUS
 from .models import Meeting_Rooms
 from .models import Meeting_Rooms_Booked
 from .models import Office_Spaces_Booked
+
+# Register your models here.
+
+admin.site.register(Office_Spaces)
+admin.site.register(Category)
+admin.site.register(STATUS)
+admin.site.register(Meeting_Rooms)
+admin.site.register(Meeting_Rooms_Booked)
+admin.site.register(Office_Spaces_Booked)
