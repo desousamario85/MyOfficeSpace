33fc0d5e04e59d749c5982bced2a90c3c64491cc
desousamario85 <desousa.mario@yahoo.com>
2022-08-27 19:22:58 +0000

    fix: error with invalid allowed host on heroku

--- office_spaces/settings.py
+++ office_spaces/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['myofficespace.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['myofficespace.herokuapp.com', 'localhost']
 
 
 # Application definition
