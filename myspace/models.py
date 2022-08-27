51449a5d75b8a5dd5df2b07f5648b79acfc2abb9
desousamario85 <desousa.mario@yahoo.com>
2022-08-27 16:34:06 +0000

    Started implementing data model

--- myspace/models.py
+++ myspace/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class category(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    category_name = models.CharField(max_length=20)
+    def __str__(self):
+        return (self.category_name)
+
+class office_spaces(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    office_name = models.CharField(max_length=50)
+    category_id = models.ForeignKey(
+        category,
+        on_delete=models.CASCADE
+        )
+    def __str__(self):
+        return (self.office_name)
+
+class status(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    status_name = models.CharField(max_length=10)
+    def __str__(self):
+        return (self.status_name)
+
+class meeting_rooms(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    meeting_name = models.CharField(max_length=50)
+    category_id = models.ForeignKey(
+        category,
+        on_delete=models.CASCADE
+        )
+    def __str__(self):
+        return (self.meeting_name)
