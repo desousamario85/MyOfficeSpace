c065569e2eba27d9d6ce65664469cfe3b0fc11ac
desousamario85 <desousa.mario@yahoo.com>
2022-08-28 14:15:09 +0000

    Index header and footer structure setup, with css

--- office_spaces/urls.py
+++ office_spaces/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,4 +24,4 @@ urlpatterns = [
     path('', include('myspace.urls')),
     path('accounts/', include('allauth.urls'))
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
